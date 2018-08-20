3630120969644777897f262cb1a9113730c53b42
Wallace <GWALLACE@MITRE.ORG>
2018-08-20 16:39:20 -0400

    msg

--- update_site.bash
+++ update_site.bash
@@ -4,8 +4,8 @@ git add *
 git commit -m "msg"
 git push
 cd ~/.ssh
-ssh root@142.93.204.107 #<< EOF
-	#cd website
-	#git pull
-	#pm2 restart app.js
-#EOF
+ssh root@142.93.204.107 << EOF
+	cd website
+	git pull
+	pm2 restart app.js
+EOF
